58cff133b0af8ccb52788653a7f2456803b3791c
matipina <mlpina@uc.cl>
2025-12-05 06:32:33 -0500

    Add placeholder pages

--- projects/project7/sketch.js
+++ projects/project7/sketch.js
@@ -9,8 +9,8 @@ function setup() {
 function draw() {
     background(250, 240, 255);
     
-    for (let x = 0; x < width; x += 50) {
-        for (let y = 0; y < height; y += 50) {
+    for (let x = 0; x <= width; x += 50) {
+        for (let y = 0; y <= height; y += 50) {
             let d = dist(mouseX, mouseY, x, y);
             let size = map(d, 0, 200, 40, 5);
             fill(108, 92, 231);
